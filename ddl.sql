22ecf87f039c56ff1ae591347747ed3f8d6d6330
mattand <mjande@umich.edu>
2024-02-14 18:37:00 -0500

    Add sql files

---
+++ ddl.sql
@@ -0,0 +1,131 @@
+/*
+Project:    Soul Plane
+Team:       Team 107
+Partners:   Paul Nguyen, Matt Anderson
+File:       Database DDL
+Date:       2/14/2024
+Author:     Paul Nguyen, Matt Anderson
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+
+-- Create Airports Table
+DROP TABLE IF EXISTS Airports;
+CREATE TABLE Airports (
+    airport_id INT PRIMARY KEY AUTO_INCREMENT,
+    airport_name VARCHAR(255) NOT NULL,
+    airport_code VARCHAR(3) UNIQUE NOT NULL,
+    location VARCHAR(255) NOT NULL
+);
+
+-- Insert values into Airports table
+INSERT INTO Airports (airport_name, airport_code, location) VALUES
+('Portland International Airport', 'PDX', 'Portland, OR'),
+('Seattle-Tacoma International Airport', 'SEA', 'SeaTac, WA'),
+('Spokane International Airport', 'GEG', 'Spokane, WA');
+
+-- Create Plane_types Table
+DROP TABLE IF EXISTS Plane_types;
+CREATE TABLE Plane_types (
+    plane_type_id INT PRIMARY KEY AUTO_INCREMENT,
+    type_name VARCHAR(255) NOT NULL,
+    capacity INT NOT NULL,
+    range_in_hrs INT NOT NULL
+);
+
+-- Insert values into Plane_types table
+INSERT INTO Plane_types (type_name, capacity, range_in_hrs) VALUES
+('Airbus A320-200', 180, 5),
+('Boeing B737-800', 190, 5),
+('Embraer 135', 37, 3);
+
+-- Create Planes Table
+DROP TABLE IF EXISTS Planes;
+CREATE TABLE Planes (
+    plane_id INT PRIMARY KEY AUTO_INCREMENT,
+    plane_type_id INT NOT NULL,
+    current_airport_id INT,
+    FOREIGN KEY (plane_type_id) REFERENCES Plane_types(plane_type_id),
+    FOREIGN KEY (current_airport_id) REFERENCES Airports(airport_id)
+    ON UPDATE CASCADE
+);
+
+-- Insert values into Planes table
+INSERT INTO Planes (current_airport_id, plane_type_id) VALUES
+(2, 2),
+(3, 1),
+(1, 3)
+(NULL, 3);
+
+-- Create Flights Table
+DROP TABLE IF EXISTS Flights;
+CREATE TABLE Flights (
+    flight_id INT PRIMARY KEY AUTO_INCREMENT,
+    plane_id INT NOT NULL,
+    depart_airport_id INT NOT NULL,
+    arrive_airport_id INT NOT NULL,
+    depart_time DATETIME NOT NULL,
+    arrive_time DATETIME NOT NULL,
+    FOREIGN KEY (plane_id) REFERENCES Planes(plane_id),
+    FOREIGN KEY (depart_airport_id) REFERENCES Airports(airport_id),
+    FOREIGN KEY (arrive_airport_id) REFERENCES Airports(airport_id)
+    ON UPDATE CASCADE
+);
+
+-- Insert values into Flights table
+INSERT INTO Flights (depart_airport_id, arrive_airport_id, plane_id, depart_time, arrive_time) VALUES
+(1, 2, 3, '2024-02-05 14:30:00', '2024-02-11 02:15:00'),
+(2, 3, 1, '2024-03-12 18:15:00', '2024-04-02 12:30:00'),
+(3, 1, 2, '2024-04-20 09:45:00', '2024-05-22 11:15:00');
+
+
+-- Create Passengers_flights Table
+DROP TABLE IF EXISTS Passengers_flights;
+CREATE TABLE Passengers_flights (
+    passenger_id INT NOT NULL,
+    flight_id INT NOT NULL,
+    PRIMARY KEY (passenger_id, flight_id),
+    FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id),
+    FOREIGN KEY (flight_id) REFERENCES Flights(flight_id)
+    ON UPDATE CASCADE
+);
+
+-- Insert values into Passengers_flights table
+INSERT INTO Passengers_flights (flight_id, passenger_id) VALUES
+(2, 1),
+(3, 2),
+(1, 3);
+
+-- Create Passengers Table
+DROP TABLE IF EXISTS Passengers;
+CREATE TABLE Passengers (
+    passenger_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state_abbr VARCHAR(2) NOT NULL,
+    zip_code VARCHAR(9) NOT NULL,
+    passport_number VARCHAR(20) UNIQUE NOT NULL
+);
+
+-- Insert values into Passengers table
+INSERT INTO Passengers (first_name, last_name, phone, email, address, city, state_abbr, zip_code, passport_number) VALUES
+('Paul', 'Nguyen', '111-222-3333', 'paul@oregonstate.edu', '1111 Oregon St', 'Corvallis', 'OR', '97330', 'A11222333'),
+('Matt', 'Anderson', '222-333-4444', 'matt@oregonstate.edu', 'B222 Washington St', 'Spokane', 'WA', '99201', '222333444'),
+('Steve', 'Rogers', '832-424-8060', 'steve@marvel.com', '14520 Walt St', 'Orlando', 'FL', '32789', 'C334445555');
+
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
+
+select * from Airports;
+select * from Plane_types;
+select * from Planes;
+select * from Flights;
+select * from Passengers_flights;
+select * from Passengers;
---
+++ dml.sql
@@ -0,0 +1,174 @@
+/*
+Project:    Soul Plane
+Team:       Team 107
+Partners:   Paul Nguyen, Matt Anderson
+File:       Database DML
+Date:       2/14/2024
+Author:     Paul Nguyen, Matt Anderson
+*/
+
+-- NOTE: All values preceded by a :colon indicate variables that will have the appropriate value when processed by backend code
+
+/* Queries to populate dropdowns for creating and updating other entities */
+
+-- Get plane IDs and plane types for plane id dropdown
+SELECT Planes.plane_id, Plane_types.type_name FROM Planes
+    JOIN Plane_types ON Planes.plane_type_id = Plane_types.plane_type_id;
+
+-- Get all airport IDs and names to populate Departing Airport, Arriving Airport, and Current Airport dropdowns
+SELECT airport_id, airport_name FROM Airports;
+
+-- Get all plane type IDs and type_names for plane type dropdown
+SELECT plane_type_id, type_name FROM Plane_types;
+
+-- Get passenger IDs, and first and last names for passenger dropdown
+SELECT passenger_id, first_name, last_name FROM Passengers;
+
+-- Get flight_id and departure and arrival airports for flight dropdown
+SELECT Flights.flight_id, DepartAirport.airport_name AS "Departure Airport", ArriveAirport.airport_name AS "Arrival Airport" FROM Flights
+    JOIN Airports AS DepartAirport ON Flights.depart_airport_id = DepartAirport.airport_id
+    JOIN Airports AS ArriveAirport ON Flights.arrive_airport_id = ArriveAirport.airport_id;
+
+
+/* Flights */
+
+-- Get all flights 
+SELECT flight_id, Flights.plane_id, Plane_types.type_name AS "Plane Type", DepartAirport.airport_name AS "Departure Airport", ArriveAirport.airport_name AS "Arrival Airport", depart_time, arrive_time FROM Flights
+    JOIN Airports AS DepartAirport ON Flights.depart_airport_id = DepartAirport.airport_id
+    JOIN Airports AS ArriveAirport ON Flights.arrive_airport_id = ArriveAirport.airport_id
+    JOIN Planes ON Flights.flight_id = Planes.plane_id
+    JOIN Plane_types ON Planes.plane_type_id = Plane_types.plane_type_id;
+
+-- Create a new flight 
+INSERT INTO Flights (plane_id, depart_airport_id, arrive_airport_id, depart_time, arrive_time)
+    VALUES (:plane_id_dropdown_input, 
+    :depart_airport_id_dropdown_input,
+    :arrive_airport_id_dropdown_input,
+    :depart_time_input,
+    :arrive_time_input
+    );
+
+-- Update a flight
+UPDATE Flights 
+    SET depart_airport_id = :depart_airport_id_dropdown_input,
+    arrive_airport_id = :arrive_airport_id_dropdown_input,
+    depart_time = :depart_time_input,
+    arrive_time = :arrive_time_input
+    WHERE flight_id = :flight_id_from_form;
+
+-- Delete a flight
+DELETE FROM Flights WHERE flight_id = :flight_id_selected_from_browse_flights_page
+
+
+/* Airports */
+
+-- Get all airports
+SELECT * FROM Airports;
+
+-- Create a new airport
+INSERT (airport_name, airport_code, location) 
+    VALUES (:airport_name_input, :airport_code_input, :location_input);
+
+-- Update an airport
+UPDATE Airports
+    SET airport_name = :airport_name_input,
+    airport_code = :airport_code_input,
+    location = :location_input
+    WHERE airport_id = :airport_id_from_form;
+
+-- Delete an airport
+DELETE FROM Airports WHERE airport_id = :airport_id_selected_from_browse_airports_page
+
+
+/* Passengers */
+
+-- Get all passengers
+SELECT * FROM Passengers;
+
+-- Create a passenger
+INSERT INTO Passengers (first_name, last_name, phone, email, address, city, state_abbr, zip_code, passport_number)
+    VALUES (:first_name_input, :last_name_input, :phone_input, :email_input, :address_input, :city_input, :state_abbr_input, :zip_code_input, :passport_number_input);
+
+-- Update a passenger
+UPDATE Passengers
+    SET first_name = :first_name_input,
+    last_name = :last_name_input,
+    phone = :phone_input,
+    email = :email_input,
+    address = :address_input,
+    city = :city_input,
+    state_abbr = :state_abbr_input,
+    zip_code = :zip_code_input,
+    passport_number = :passport_number_input
+    WHERE passenger_id = :passenger_id_from_form;
+
+-- Delete a passenger
+DELETE FROM Passengers WHERE passenger_id = :passenger_id_selected_from_browse_passengers_page
+
+/* Planes */
+
+-- Get all planes
+SELECT plane_id, Plane_types.type_name AS "Plane Type", Airports.airport_name AS "Current Airport" FROM Planes
+    JOIN Plane_types ON Planes.plane_type_id = Plane_types.plane_type_id
+    JOIN Airports ON Planes.current_airport_id = Airports.airport_id;
+
+-- Create a new plane
+INSERT INTO Planes (plane_type_id, current_airport_id)
+    VALUES (:plane_type_dropdown_input, :current_airport_dropdown_input);
+
+-- Update a plane 
+UPDATE Planes
+    SET plane_type_id = :plane_id_dropdown_input,
+    current_airport_id = :current_airport_dropdown_input
+    WHERE plane_id = :plane_id_from_form;
+
+-- Update a plane to maintenance status by setting its current airport to NULL
+UPDATE Planes
+    SET current_airport_id = NULL
+    WHERE plane_id = :plane_id_from_form;
+
+-- Delete a plane
+DELETE FROM Planes WHERE plane_id = :plane_id_selected_from_browse_planes_page
+
+/* Plane types */
+
+-- Get all plane types
+SELECT * FROM Plane_types;
+
+-- Create a new plane type
+INSERT INTO Plane_types (type_name, capacity, range_in_hrs)
+    VALUES (:type_name_input, :capacity_input, :range_in_hrs_input);
+
+-- Update a plane type
+UPDATE Plane_types
+    SET type_name = :type_name_input,
+    capacity = :capacity_input,
+    range_in_hrs = :range_in_hrs_input
+    WHERE plane_type_id = :plane_type_id_from_form
+
+-- Delete a plane type
+DELETE FROM Plane_types WHERE plane_type_id = :plane_type_id_selected_from_browse_plane_types_page
+
+
+/* Passengers Flights */
+
+-- Get all flights for each passenger
+SELECT Passengers.first_name, Passengers.last_name, DepartAirport.airport_name, ArriveAirport.airport_name, Flights.depart_time, Flights.arrive_time FROM Passengers_flights
+    JOIN Passengers ON Passengers_flights.passenger_id = Passengers.passenger_id
+    JOIN Flights ON Passengers_flights.flight_id = Flights.flight_id
+    JOIN Airports AS DepartAirport ON Flights.depart_airport_id = DepartAirport.airport_id
+    JOIN Airports AS ArriveAirport ON Flights.arrive_airport_id = ArriveAirport.airport_id;
+
+-- Associate a passenger with a flight (M:M relationship addition)
+INSERT INTO Passengers_flights (passenger_id, flight_id)
+    VALUES (:passenger_dropdown_input, :flight_dropdown_input);
+
+-- Update the relationship between a passenger and a flight (M:M relationship update)
+UPDATE Passengers_flights
+    SET passenger_id = :passenger_dropdown_input,
+    flight_id = :flight_dropdown_input;
+
+-- Dis-associate a passenger with a flight (M:M relationship deletion)
+DELETE FROM Passengers_flights 
+    WHERE passenger_id = :passenger_id_selected_from_browse_passengers_flights_page
+    AND flight_id = :flight_id_selected_from_browse_passengers_flights_page
